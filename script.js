02f2827e2dff8fd1f7d50f73a089d63133d503d1
campbelle-dev <ckcheunteu@gmail.com>
2026-03-20 12:21:25 +0100

    Premier commit : structure de base de la To-Do List

---
+++ script.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('addBtn');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if(taskText !== "") {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+});
